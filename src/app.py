f2223e8703110ee49443920397576c9cda77da28
Ivan Souza <57714853+ivanlucasm@users.noreply.github.com>
2025-03-24 16:01:55 +0000

    Adiciona validação para impedir inscrições em atividades lotadas

--- src/app.py
+++ src/app.py
@@ -98,6 +98,9 @@ def signup_for_activity(activity_name: str, email: str):
 
     # Get the specificy activity
     activity = activities[activity_name]
+    #Validate student is not already signed up
+    if len(activity["participants"]) >= activity["max_participants"]:
+        raise HTTPException(status_code=400, detail="Activity is full")
 
     # Validar se o aluno já está inscrito       
     if email in activity["participants"]:
